f8cf16968fb7b9eb385b3d87c5bca97f4b51c167
gzy <gzyonline@hotmail.com>
2022-10-26 17:18:57 +0800

    jsx support

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <template>
-  <div id="app">{{ msg }}</div>
+  <div id="app">
+    <div>{{ msg }}</div>
+    <SubTitle></SubTitle>
+  </div>
 </template>
 
-<script setup>
+<script setup lang="jsx">
+import { defineComponent } from 'vue';
+
 const msg = 'Hello World!';
+
+const SubTitle = defineComponent({
+  name: '',
+  render() {
+    return <div>This is first jsx demo.</div>;
+  },
+});
 </script>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
+import vueJsx from '@vitejs/plugin-vue2-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx()
+  ]
 })
