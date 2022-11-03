41629be547c23546e74e0210c97e261621d4bd0f
gzy <gzyonline@hotmail.com>
2022-11-03 08:13:28 +0800

    add remove-console plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue2-jsx'
+import removeConsole from 'vite-plugin-remove-console'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx()
+    vueJsx(),
+    removeConsole(),
   ]
 })
