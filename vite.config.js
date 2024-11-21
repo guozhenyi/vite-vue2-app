b7dd70820dd37ea1de4646e145ccf14c8817dfc6
gzy <gzyonline@hotmail.com>
2024-11-21 16:28:44 +0800

    add vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue2'
 import vueJsx from '@vitejs/plugin-vue2-jsx'
 import removeConsole from 'vite-plugin-remove-console'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     removeConsole(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'Vite vue2 template'
+        }
+      }
+    })
   ],
   resolve: {
     alias: {
